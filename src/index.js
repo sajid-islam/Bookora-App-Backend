ee3e2d1787da9ccc2d281980bef5ae52698274b9
sajid-islam <sajidislam941@gmail.com>
2025-07-08 22:03:55 +0600

    create mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import "dotenv/config";
 
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 const port = 3000 || process.env.PORT;
 
-app.listen(port, () => console.log("SERVER RUNNING ON PORT", port));
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+    console.log("SERVER RUNNING ON PORT", port);
+    connectDB();
+});
