c291205fcd7d0d1282342b959ee8d78f7fa17696
sajid-islam <sajidislam941@gmail.com>
2025-07-14 20:01:45 +0600

    fix(auth.middleware.js): correct import path of user model

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "./../models/User";
+import User from "./../models/User.js";
 import "dotenv/config";
 
 const protectRoute = async (req, res, next) => {
